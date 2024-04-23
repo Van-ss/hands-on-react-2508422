c2e6b5b73fb23866f0ed22cb2006a1c48c0ba48a
Vanshika <85989897+Van-ss@users.noreply.github.com>
2024-04-23 06:48:07 +0000

    finished exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 function App() {
+  const name= 'StarGazers'
+
   return (
+    <div className="container">
+      <article>
     <hgroup>
-      <h1>Meet the StarGazers</h1>
+      <img src="images/group.svg" alt="StarGazers Group" />
+      <h1>Meet the <i style={{color:"steelblue"}}>{name}</i></h1>
       <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+      <button className="outline" onClick={() => alert('Hi There')}>Click Me</button>
     </hgroup>
+    </article>
+    </div>
   )
 }
 export default App
